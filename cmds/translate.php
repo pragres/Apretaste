b527d702f258e86d3bfa7c3b01a91e56e7f1ff6e
rafa <rrodriguezramirez@gmail.com>
2014-03-23 15:12:52 -0500

    bugifxs translator

--- cmds/translate.php
+++ cmds/translate.php
@@ -114,7 +114,7 @@ function cmd_translate($robot, $from, $argument, $body = '', $images = array()){
 		
 		$robot->log("Detecting language...");
 		
-		$url = "http://translate.google.com/translate_a/t?client=t&sl={$lfrom}&tl={$lto}&hl={$hl}&sc=2&ie=WINDOWS-1252&oe=UTF-8&oc=13&otf=2&ssel=3&tsel=6&q=" . rawurlencode(utf8_decode($text));
+		$url = "http://translate.google.com/translate_a/t?client=t&sl={$lfrom}&tl={$lto}&hl={$hl}&sc=2&ie=WINDOWS-1252&oe=UTF-8&oc=13&otf=2&ssel=3&tsel=6&q=" .$text;
 		$robot->log($url, "URL");
 		$json = file_get_contents($url);
 		$json = div::jsonDecode($json);

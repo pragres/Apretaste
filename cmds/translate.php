8ae51b77ff64686fc09f6e75fb60714cd84c7cca
rafa <rrodriguezramirez@gmail.com>
2014-03-23 17:15:19 -0500

    bugifxs translator: encoding problems

--- cmds/translate.php
+++ cmds/translate.php
@@ -112,7 +112,7 @@ function cmd_translate($robot, $from, $argument, $body = '', $images = array()){
 		
 		$robot->log("Detecting language...");
 		
-		$url = "http://translate.google.com/translate_a/t?client=t&sl=auto&tl={$lto}&hl={$hl}&sc=2&ie=UTF-8&oe=UTF-8&oc=13&otf=2&ssel=3&tsel=6&q=" . urlencode($text);
+		$url = "http://translate.google.com/translate_a/t?client=t&sl=auto&tl={$lto}&hl={$hl}&sc=2&ie=UTF-8&oe=UTF-8&oc=13&otf=2&ssel=3&tsel=6&q=" . urlencode(html_entity_decode(cmd_translate_fix_text($text)));
 		$robot->log($url, "URL");
 		$json = file_get_contents($url);
 		$json = div::jsonDecode($json);
@@ -141,11 +141,12 @@ function cmd_translate($robot, $from, $argument, $body = '', $images = array()){
 	else
 		$oe = 'UTF-8';
 	
-	$url = "http://translate.google.com/translate_a/t?client=t&sl={$lfrom}&tl={$lto}&hl={$hl}&sc=2&ie=$ie&oe=$oe&oc=13&otf=2&ssel=3&tsel=6&q=" . urlencode($text);
+	$url = "http://translate.google.com/translate_a/t?client=t&sl={$lfrom}&tl={$lto}&hl={$hl}&sc=2&ie=$ie&oe=$oe&oc=13&otf=2&ssel=3&tsel=6&q=" . urlencode(html_entity_decode(cmd_translate_fix_text($text)));
 	$robot->log($url, "URL");
 	$json = file_get_contents($url);
 	
-	if (Apretaste::isUTF8($json)) $json = utf8_encode($json);
+	if (Apretaste::isUTF8($json))
+		$json = utf8_encode($json);
 	
 	$arr = div::jsonDecode($json); // uso este metodo porque la funcion de php no sirve
 	
@@ -314,7 +315,6 @@ function parse_google_translator_response($response){
 			"meanings" => $meaninghtml
 	);
 }
-
 function cmd_translate_fix_text($text){
 	if (! Apretaste::isUTF8($text))
 		$text = utf8_encode($text);

e93320a317e2adbd6fae57ceeee4c8d590b661d5
rafa <rrodriguezramirez@gmail.com>
2014-03-23 01:13:21 -0500

    bugifxs translator

--- cmds/remedy.php
+++ cmds/remedy.php
@@ -4,6 +4,15 @@ function cmd_remedy($robot, $from, $argument, $body = '', $images = array()){
 	
 	if (isset($results[0])) {
 		$remedio = file_get_contents($results[0]->url);
+		
+		$doc = new DOMDocument();
+		
+		@$doc->loadHTML($remedio);
+		
+		$r = $doc->getElementById("layer5");
+		
+		$remedio = $doc->saveHTML($r);
+		
 		$titulo = $results[0]->title;
 		unset($results[0]);
 		return array(

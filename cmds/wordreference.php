6e02c1ccfd42f29c08def59b77aa25c8c4220a62
rafa <rrodriguezramirez@gmail.com>
2014-03-25 23:10:09 -0500

    improve weather service

--- cmds/wordreference.php
+++ cmds/wordreference.php
@@ -3,22 +3,26 @@ function cmd_wordreference($robot, $from, $argument, $body = '', $images = array
 	if (! Apretaste::isUTF8($argument))
 		$argument = utf8_encode($argument);
 	
-	if (substr($argument, 0, 3) == 'ID:')
+	if (substr($argument, 0, 3) == 'ID:') {
+		$robot->log("Getting word reference $argument in Real Academia Espanola");
 		$result = file_get_contents("http://buscon.rae.es/drae/srv/search?id=" . urlencode(substr($argument, 3)));
-	else
+	} else {
+		$robot->log("Searching for $argument in Real Academia Espanola");
 		$result = file_get_contents("http://buscon.rae.es/drae/srv/search?val=" . urlencode($argument));
+	}
 	
-	
-	$result = str_replace("search?id=","mailto:".'{$reply_to}'."?subject=SIGNIFICADO ID:", $result);
+	$robot->log("Cleanning the result");
 	
 	if (! Apretaste::isUTF8($result))
 		$result = utf8_encode($result);
+	
+	$result = str_replace("search?id=", "mailto:" . '{$reply_to}' . "?subject=SIGNIFICADO ID:", $result);
 	
 	return array(
 			"answer_type" => "wordreference",
 			"command" => "wordreference",
 			"title" => "Significados: $argument",
-			"result" => $page,
+			"result" => $result,
 			"compactmode" => true
 	);
 }

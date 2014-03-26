c6c6dffca49fc1f1fa51d466f191810bd4d7bd3d
rafa <rrodriguezramirez@gmail.com>
2014-03-25 23:04:50 -0500

    improve weather service

--- cmds/wordreference.php
+++ cmds/wordreference.php
@@ -4,9 +4,9 @@ function cmd_wordreference($robot, $from, $argument, $body = '', $images = array
 		$argument = utf8_encode($argument);
 	
 	if (substr($argument, 0, 3) == 'ID:')
-		$result = file_get_contents("http://buscon.rae.es/drae/srv/search?id=" . url_encode(substr($argument, 3)));
+		$result = file_get_contents("http://buscon.rae.es/drae/srv/search?id=" . urlencode(substr($argument, 3)));
 	else
-		$result = file_get_contents("http://buscon.rae.es/drae/srv/search?val=" . url_encode($argument));
+		$result = file_get_contents("http://buscon.rae.es/drae/srv/search?val=" . urlencode($argument));
 	
 	
 	$result = str_replace("search?id=","mailto:".'{$reply_to}'."?subject=SIGNIFICADO ID:", $result);

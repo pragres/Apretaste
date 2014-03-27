08535644ba15d26d50da87b794b200af3beb3d1d
rafa <rrodriguezramirez@gmail.com>
2014-03-27 17:51:45 -0500

    improve sudoku

--- cmds/sudoku.php
+++ cmds/sudoku.php
@@ -78,10 +78,10 @@ function print_sudoku($sudoku, $for_print = false){
 				if ($for_print) {
 					$html .= '&nbsp;';
 				} else {
-					$html .= '<!--[if !mso ]><select style="padding: 3px; border: 1px solid gray;"><option value="-">&nbsp;</option>';
+					$html .= '<select style="padding: 3px; border: 1px solid gray;"><option value="-">&nbsp;</option>';
 					for($i = 1; $i <= 9; $i ++)
 						$html .= '<option value="' . $i . '">' . $i . '</option>';
-					$html .= '</select><![endif]-->';
+					$html .= '</select>';
 				}
 			} else
 				$html .= $v;

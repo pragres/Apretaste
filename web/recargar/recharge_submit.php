031fe4efd8fdbf364eb5cf661afec101a63c1e3a
rafa <rrodriguezramirez@gmail.com>
2014-05-03 17:03:42 -0400

    eMerge hackaton: sync work

--- web/recargar/recharge_submit.php
+++ web/recargar/recharge_submit.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -11,28 +10,25 @@ $customer_email = $_POST['customer_email'];
 $user_email = $_POST['user_email'];
 $amount = $_POST['amount'];
 
-
 // Charge the customer's credit card
-require_once('../lib/Stripe/Stripe.php');
+require_once ('../lib/Stripe/Stripe.php');
 Stripe::setApiKey("sk_test_xCTtSZAffzCkUolsHhBvZUrQ");
 $token = $stripeToken;
 try {
 	$charge = Stripe_Charge::create(array(
-		"amount" => 1000, // amount in cents, again
-		"currency" => "usd",
-		"card" => $token,
-		"description" => "payinguser@example.com")
-	);
-} catch(Stripe_CardError $e) {
+			"amount" => $amount * 100,
+			"currency" => "usd",
+			"card" => $token,
+			"description" => "payinguser@example.com"
+	));
+} catch ( Stripe_CardError $e ) {
 	// error message
 	// @TODO send an alert to our emails
 	die('Lo sentimos mucho, pero ha habido un error cobrando. Su tarjeta de credito no ha sido cargada. Por favor presione "Atras" en su navegador e intente nuevamente. Si no funciona, escribanos a soporte@apretaste.com y le ayudaremos al momento.');
 }
 
-
 // send confirmation email to the customOer
 // @TODO RAFA ADD YOUR CODE TO SEND EMAIL HERE
-
 
 // send alert email to the user
 // @TODO RAFA ADD YOUR CODE TO SEND EMAIL HERE
@@ -40,10 +36,8 @@ try {
 // Add the funds to the user
 ApretasteMoney::recharge($customer_email, $amount);
 
-
 // create payment log
 // @TODO RAFA ADD YOUR CODE TO CREATE THE LOG
-
 
 // redirect to the thank you page
 header("thank_you.php?user=$user_email&customer=$customer_email&amount=$amount");

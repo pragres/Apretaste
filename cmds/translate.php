18ed64475f3eafbe1a938af7608b9ef8ab72012f
rafa <rrodriguezramirez@gmail.com>
2014-05-11 19:59:01 -0400

    improve translator

--- cmds/translate.php
+++ cmds/translate.php
@@ -176,12 +176,12 @@ function cmd_translate($robot, $from, $argument, $body = '', $images = array()){
 	
 	$robot->log("Translating the text with Google Translator from -$lfrom- to -$lto-...");
 	
-	$url = "http://translate.google.com/translate_a/t?client=t&sl={$lfrom}&tl={$lto}&hl={$hl}&sc=2&ie=UTF-8&oe=UTF-8&oc=13&otf=2&ssel=3&tsel=6&q=" . urlencode($text);
+	$url = "http://translate.google.com/translate_a/t?client=t&sl={$lfrom}&tl={$lto}&hl={$hl}&sc=2&ie=UTF-8&oe=UTF-8&oc=13&otf=2&ssel=3&tsel=6&q=" . $text;
 	
 	$robot->log($url, "URL");
 	
 	$json = file_get_contents($url);
-	
+	ec
 	if (! Apretaste::isUTF8($json))
 		$json = utf8_encode($json);
 	

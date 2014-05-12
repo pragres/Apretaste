f8dedac0e6fd876a305ffaf558c7f27c00616f3d
rafa <rrodriguezramirez@gmail.com>
2014-05-11 20:18:34 -0400

    improve translator

--- cmds/translate.php
+++ cmds/translate.php
@@ -176,7 +176,7 @@ function cmd_translate($robot, $from, $argument, $body = '', $images = array()){
 	
 	$robot->log("Translating the text with Google Translator from -$lfrom- to -$lto-...");
 	
-	$url = "http://translate.google.com/translate_a/t?client=t&sl={$lfrom}&tl={$lto}&hl={$hl}&sc=2&ie=UTF-8&oe=UTF-8&oc=13&otf=2&ssel=3&tsel=6&q=" . url_encode($text);
+	$url = "http://translate.google.com/translate_a/t?client=t&sl={$lfrom}&tl={$lto}&hl={$hl}&sc=2&ie=UTF-8&oe=UTF-8&oc=13&otf=2&ssel=3&tsel=6&q=" . urlencode($text);
 	
 	$robot->log($url, "URL");
 	

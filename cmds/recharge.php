b523c71858e4d1f2494956f8018147461640f1b2
rafa <rrodriguezramirez@gmail.com>
2014-05-28 22:15:04 -0400

    new command: recharge

---
+++ cmds/recharge.php
@@ -0,0 +1,42 @@
+<?php
+function cmd_recharge($robot, $from, $argument, $body = '', $images = array()){
+	$robot->log("Recharge credit for $from with $argument card");
+	
+	$argument = trim($argument);
+	
+	$code = str_replace("'", "''", $argument);
+	
+	$r = Apretaste::query("SELECT * FROM recharge_card WHERE code = '$code' AND email is null;");
+	
+	$from = strtolower($from);
+	
+	$credit = ApretasteMoney::getCreditOf($from);
+	
+	if (isset($r[0]))
+		if ($r[0]['code'] == $argument) {
+			
+			Apretaste::query("UPDATE recharge_card SET email = '$from' WHERE code = '$code';");
+			
+			$newcredit = ApretasteMoney::getCreditOf($from);
+			
+			if ($newcredit > $credit) {
+				return array(
+						"answer_type" => "recharge_successfull",
+						"amount" => $r[0]['amount'],
+						"from" => $from
+				);
+			}
+			
+			return array(
+				"answer_type" => "recharge_fail",
+				"code" => $code,
+				"from" => $from
+			);
+		}
+	
+	return array(
+			"answer_type" => "recharge_card_wrong",
+			"code" => $code,
+			"from" => $from
+	);
+}
--- tpl/alone/recharge_successfull.tpl
+++ tpl/alone/recharge_successfull.tpl
@@ -1,6 +1,5 @@
 {= AnswerSubject: "Su cuenta ha sido recargada satisfactoriamente" =}
-<h1 style="margin-top: 0px;">Su cuenta ha sido recargada satisfactoriamente</h1>
-<p>Su cuenta ha sido recargada satisfactoriamente con <b>{$amount} USD</b>. 
-<p>Nuevamente:</p>
-<p><b>Gracias por usar Apretaste!</b></p>
-
+{$h1}Su cuenta ha sido recargada satisfactoriamente{$_h1}
+{$p}Su cuenta ha sido recargada satisfactoriamente con <b>{$amount} USD</b>. {$_p}
+{$p}Nuevamente:{$_p}
+{$p}<b>Gracias por usar Apretaste!</b>{$p}

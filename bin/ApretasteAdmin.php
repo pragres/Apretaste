e8bf2af5243bfdc9941c8e9b222d3f1bdd35e82f
rafa <rrodriguezramirez@gmail.com>
2014-06-04 22:13:31 -0400

    improve admin & email robot

--- bin/ApretasteAdmin.php
+++ bin/ApretasteAdmin.php
@@ -1002,7 +1002,7 @@ class ApretasteAdmin {
 			
 			$data['ad'] = Apretaste::getAnnouncement($id);
 			if ($data['ad'] != APRETASTE_ANNOUNCEMENT_NOTFOUND) {
-				$data['ad']['image'] = '<img width="200" src="data:' . $data['ad']['image_type'] . ';base64,' . $data['ad']['image'] . '">';
+				$data['ad']['image'] = false; //'<img width="200" src="data:' . $data['ad']['image_type'] . ';base64,' . $data['ad']['image'] . '">';
 			} else{
 				$data['ad'] = false;
 				$data['notfound'] = true;

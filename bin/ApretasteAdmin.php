84c1064e21e105a5f2b4d8d5f01165ddc7c11194
Rafael Rodríguez Ramírez <rafa@pragres.com>
2014-06-28 15:28:06 -0400

    Bug on submit an image in admin/raffles

--- bin/ApretasteAdmin.php
+++ bin/ApretasteAdmin.php
@@ -1029,8 +1029,9 @@ class ApretasteAdmin {
 			$df = $_POST['date_from'];
 			$dt = $_POST['date_to'];
 			
-			if (isset($_FILES['image']))
-				$image = base64_encode(file_get_contents($_FILES['image']['tmp_name']));
+			if (trim("{$_FILES['image']['tmp_name']}") !="")
+				if (isset($_FILES['image']))
+					$image = base64_encode(file_get_contents($_FILES['image']['tmp_name']));
 			
 			Apretaste::query("INSERT INTO raffles (description, date_from, date_to, image)
 					VALUES ('$desc', '$df', '$dt', '$image');");

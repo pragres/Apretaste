165bfc45935d5c0e753d13ebca0db02e75144ef3
rafa <rrodriguezramirez@gmail.com>
2014-08-05 22:43:44 -0400

    several bugs and improvements

--- admin/agency_recharge.php
+++ admin/agency_recharge.php
@@ -1,5 +1,4 @@
 <?php
-
 $email = post("edtEmail");
 $author = Apretaste::getAuthor($email);
 
@@ -13,10 +12,11 @@ if (! is_null($email)) {
 		RETURNING id;");
 	
 	$id = $r[0]['id'];
-	
 }
 
 $r = Apretaste::query("SELECT * FROM agency_recharge WHERE id = '$id';");
 
 $data['recharge'] = $r[0];
 $data['author'] = $author;
+
+header("Location: index.php?path=admin&page=agency_customer&id=" . $customer);

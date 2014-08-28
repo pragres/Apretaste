8aa99fc9fc56f84241ee163df297827f59cd8efc
rafa <rrodriguezramirez@gmail.com>
2014-08-28 15:49:41 -0400

    several bugs and improvements

---
+++ admin/config_add_agency.php
@@ -0,0 +1,21 @@
+<?php
+if (post('btnAddAgency', false)) {
+	
+	$name = str_replace("'", "''", post('edtName'));
+	$phone = str_replace("'", "''", post('edtPhone'));
+	$credit_line = post('edtCreditLine');
+	$address = str_replace("'", "''", post('edtAddress'));
+	$profit_percent = post('edtProfitPercent');
+	$residual_percent = post('edtResidualPercent');
+	
+	if (empty($profit_percent))
+		$profit_percent = "null";
+	
+	if (empty($residual_percent))
+		$residual_percent = "null";
+	
+	q("INSERT INTO agency (name,phone,credit_line,address,profit_percent,residual_percent) 
+			VALUES ('$name','$phone','$credit_line','$address', $profit_percent,$residual_percent);");
+	
+	header("Location: index.php?path=admin&page=config_agency");
+}
--- admin/config_agency.php
+++ admin/config_agency.php
@@ -18,9 +18,24 @@ if (isset($_POST['btnUpdateAgency'])) {
 	Apretaste::setConfiguration("agency_residual_profit", $profit);
 }
 
+if (post("btnUpdateAgencyPercents", false)) {
+	$profit = post('edtAgencyProfitPercent', 0.2);
+	$residual = post('edtAgencyResidualPercent', 0.05);
+	
+	if ($profit >= 1)
+		$profit = number_format($profit / 100, 2) * 1;
+	if ($residual >= 1)
+		$residual = number_format($residual / 100, 2) * 1;
+	
+	$id = post('cboAgencyPercents');
+	
+	q("update agency set profit_percent = $profit, residual_percent = $residual where id = '$id';");
+}
+
 // Load data
 $data['edtProfit'] = Apretaste::getConfiguration("agency_profit", 0);
 $data['edtResidualProfit'] = Apretaste::getConfiguration("agency_residual_profit", 0);
 $data['agents'] = Apretaste::query("SELECT * FROM users where user_role = 'agent' order by user_login;");
-$data['agency_percents'] = q("select * from agency_percents;");
-$data['agencies'] = q("select * from agency_expanded;");
+$data['agency_percents'] = q("select * from agency_percents order by name;");
+$order = get('orderby', 'name');
+$data['agencies'] = q("select * from agency_expanded order by $order;");

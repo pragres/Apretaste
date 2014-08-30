a21420b879794c3bf6cb8be80d5a11b51c7a0852
rafa <rrodriguezramirez@gmail.com>
2014-08-29 23:46:33 -0400

    several bugs and improvements

--- dev/test.php
+++ dev/test.php
@@ -2,15 +2,7 @@
 
 // put here test code
 $email = $_SERVER['argv'][1];
-echo $email;
-var_dump($_SERVER['argv']);
 
 $r = ApretasteMarketing::addSubscriber($email);
 
-var_dump($r);
-
-$r = ApretasteMarketing::getSubscriber($email);
-var_dump($r);
-
-$r = ApretasteMarketing::delSubscriber($email);
 var_dump($r);

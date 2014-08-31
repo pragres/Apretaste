3fef1a21d2989b8be6148ee3eb10632fb4929b5b
rafa <rrodriguezramirez@gmail.com>
2014-08-30 20:30:57 -0400

    several bugs and improvements

--- dev/test.php
+++ dev/test.php
@@ -3,8 +3,9 @@
 // put here test code
 Apretaste::connect();
 
-$emails = q("SELECT lower(extact_email(author)) as author from message group by author;");
+$emails = q("SELECT lower(extract_email(author)) as author from message group by author;");
 
 foreach ( $emails as $email ) {
+	echo "Adding $email\n";
 	$r = ApretasteMarketing::addSubscriber($email['author']);
 }

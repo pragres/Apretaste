d9a2cb139d198c7a0eb3de51f2c0f9b58ce58d02
rafa <rrodriguezramirez@gmail.com>
2014-09-09 00:52:01 -0400

    several bugs and improvements

--- admin/address_list.php
+++ admin/address_list.php
@@ -67,4 +67,4 @@ $data['total_address'] = $r[0]['total'];
 
 $data['providers'] = Apretaste::query("Select provider, total, case when provider ~* '.cu' then 1 else 0 end as national from (
 		SELECT get_email_domain(email) as provider, count(*) as total
-		from address_list group by provider order by total desc) as subq;");
+		from address_list group by provider order by total desc) as subq limit 100;");

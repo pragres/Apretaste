fe3f46e9d57e6f14deb2f673f442c702deb59f02
rafa <rrodriguezramirez@gmail.com>
2014-09-12 23:20:57 -0400

    several bugs and improvements

--- admin/dispatchers_reports.php
+++ admin/dispatchers_reports.php
@@ -1,4 +1,4 @@
 <?php 
 
-$data['crosstable'] = q("SELECT dispatcher, \"3\", \"5\", \"10\", total FROM dispatchers_recharges_cross;");
+$data['crosstable'] = q("SELECT dispatcher, \"3\", \"5\", \"10\", total FROM dispatchers_recharges_cross order by total desc;");
 $data['recharges_by_day'] = q("SELECT dia as day, total as recharges, amount FROM dispatchers_recharges_by_day ORDER BY dia");

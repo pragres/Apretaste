1d6199409b99f1042519a43eef3c81553be0c541
rafa <rrodriguezramirez@gmail.com>
2014-09-30 00:42:06 -0400

    several bugs and improvements

--- admin/agency_bill.php
+++ admin/agency_bill.php
@@ -1,7 +1,7 @@
 <?php
 
 $data['agency'] = ApretasteMoney::getAgency($data['user']['agency']);
-$data['weeks'] = q("select * from agency_week_details where agency = '{$data['user']['agency']}' order by year desc, month desc, week desc;");
+$data['weeks'] = q("select * from agency_weeks_without_payment where agency = '{$data['user']['agency']}' order by year desc, week desc;");
 
 if (!is_array($data['weeks']))
 	$data['weeks'] = array();

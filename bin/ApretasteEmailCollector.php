8d1e04434de9793f94f09d418d9514c6aea4dc5a
rafa <rrodriguezramirez@gmail.com>
2014-10-25 00:12:17 -0400

    update

--- bin/ApretasteEmailCollector.php
+++ bin/ApretasteEmailCollector.php
@@ -264,7 +264,9 @@ class ApretasteEmailCollector {
 					$textBody = strip_tags($htmlBody);
 				}
 				
-				if (similar_text(trim(strtolower("" . base64_decode($textBody))), trim(strtolower(strip_tags($htmlBody))), true) > 0.9) {
+				$similar = similar_text(trim(strtolower("" . base64_decode($textBody))), trim(strtolower(strip_tags($htmlBody))), $percent);
+				
+				if ( $percent > 0.9) {
 					$textBody = strip_tags($htmlBody);
 				}
 				

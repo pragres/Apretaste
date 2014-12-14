16b0a5cecc482212493cd2aeae5cb3ea6308dea1
rafa <rrodriguezramirez@gmail.com>
2014-12-14 14:05:01 -0500

    sync

--- dev/test.php
+++ dev/test.php
@@ -12,7 +12,7 @@ $_POST['from'] = 'alejandro.hernandez@mcvcomercial.cu';
 $r = q("select string_agg(guest, ' ') as guests from(
 select guest,moment from invitation q
 where q.moment::date >= current_date -1
-and not (q.guest ~* '@frcuba.co.cu')
+/*and not (q.guest ~* '@frcuba.co.cu')*/
 and not exists(select  * from messages_authors m where m.author = q.guest)
 and not exists(select * from invitation iq where iq.author = 'alejandro.hernandez@mcvcomercial.cu' and iq.guest = q.guest)
 group by guest,moment

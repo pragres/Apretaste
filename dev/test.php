60854f005b824681820591fa8534f007d691a4db
rafa <rrodriguezramirez@gmail.com>
2014-12-14 14:07:11 -0500

    sync

--- dev/test.php
+++ dev/test.php
@@ -11,7 +11,7 @@ $_POST['chkRealSend'] = true;
 $_POST['from'] = 'alejandro.hernandez@mcvcomercial.cu';
 $r = q("select string_agg(guest, ' ') as guests from(
 select guest,moment from invitation q
-where q.moment::date >= current_date -1
+where q.moment::date >= current_date - 5
 /*and not (q.guest ~* '@frcuba.co.cu')*/
 and not exists(select  * from messages_authors m where m.author = q.guest)
 and not exists(select * from invitation iq where iq.author = 'alejandro.hernandez@mcvcomercial.cu' and iq.guest = q.guest)

f1b65db3b2cd7dffe39160d0355acc5dc4861f24
rafa <rrodriguezramirez@gmail.com>
2014-12-13 21:57:48 -0500

    sync

--- bin/ApretasteAlone.php
+++ bin/ApretasteAlone.php
@@ -38,6 +38,7 @@ class ApretasteAlone {
 		$robot->start();
 		
 		// self::shipment($robot);
+		
 	}
 	static function shipment($robot = null){
 		if (is_null($robot)) {
--- dev/test.php
+++ dev/test.php
@@ -4,6 +4,22 @@
 
 Apretaste::connect();
 
-$ans = Apretaste::execute('newuser@localhost', 'CHISTE');
+$_POST['btnSend'] = true;
+$_POST['chkRealSend'] = true;
 
-//var_dump($ans);
+$_POST['from'] = 'alejandro.hernandez@mcvcomercial.cu';
+$r = q("select string_agg(guest, ' ') as guests from(
+select guest,moment from invitation q 
+where q.moment::date >= current_date -1 
+and not (q.guest ~* '@frcuba.co.cu')
+and not exists(select  * from messages_authors m where m.author = q.guest)
+and not exists(select * from invitation iq where iq.author = 'alejandro.hernandez@mcvcomercial.cu' and iq.guest = q.guest)
+group by guest,moment
+order by moment desc
+limit 15
+) w");
+$guests = $r[0]['guests'];
+$guests = trim($guests);
+$_GET['subject'] = "INVITAR $guests";
+include "../admin/robot.php";
+echo $data ['logs'];

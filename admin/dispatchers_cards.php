ff18981266d3bf9d6cd7492606ae9446cacae2af
rafa <rrodriguezramirez@gmail.com>
2015-02-01 22:33:22 -0500

    add recharge card deactivation feature

--- admin/dispatchers_cards.php
+++ admin/dispatchers_cards.php
@@ -1,5 +1,15 @@
 <?php
 
+if (isset($_GET['deactivate'])){
+    $code = $_GET['deactivate'];
+    $sale = $_GET['cards'];
+    
+    q("DELETE FROM recharge_card WHERE code = '$code';");
+    q("UPDATE recharge_card_sale SET quantity = quantity - 1 WHERE id =  '$sale';");
+
+    $data['msg'] = "The card $code was deactivated";
+    $data['msg-type'] = "success";
+}
 
 $data['cards'] = ApretasteMoney::getSaleCards($_GET['cards']);
 $data['sale'] = $_GET['cards'];

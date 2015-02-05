d246c14d5e297d624d702b8e0a1c64d63753c5d2
rafa <rrodriguezramirez@gmail.com>
2015-02-05 14:33:15 -0500

    fix new feature

--- bin/ApretasteEmailCollector.php
+++ bin/ApretasteEmailCollector.php
@@ -168,7 +168,7 @@ class ApretasteEmailCollector
 
                 if (!is_null($filter)) {
                     if (stripos($headers->subject, $filter) === false) {
-                        echo "[INFO] Ignoring message because subject was filtered by user (filter=$filter)\n";
+                        echo "[INFO] Ignoring message {$headers->subject} because subject was filtered by user (filter=$filter)\n";
                         continue;
                     }
                 }
